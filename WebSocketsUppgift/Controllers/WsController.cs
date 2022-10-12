92cabf9f683dc52be63d8583c4dcbfd5a247f595
Odenstig <erik.odenstig@live.se>
2022-10-12 21:10:18 +0200

    Add project files.

---
+++ WebSocketsUppgift/Controllers/WsController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+
+namespace WebSocketsUppgift.Controllers
+{
+    public class WsController : Controller
+    {
+        //Server
+
+        //Setting up logger to receive data sent from client in console
+        private readonly ILogger<WsController> _logger;
+
+        //Simple class for Json conversion
+        private class Text
+        {
+            public string Message { get; set; }
+        }
+
+        public WsController(ILogger<WsController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Get Task setup to receive websocket requests from client
+        [HttpGet("/ws")]
+        public async Task Get()
+        {
+            //If it's a ws request establish connection and start receiving/sending data, else throw 400 error
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                await Reader(webSocket);
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            }
+        }
+
+        //For sending/receiving data in established connection with client
+        private async Task Reader(WebSocket webSocket)
+        {
+
+            //Creates a new object to be serialized 
+            var establishedMessage = new Text()
+            {
+                Message = "Server ready to receive data!"
+            };
+
+            //Serialize above object
+            var establishedJson = JsonSerializer.Serialize(establishedMessage);
+
+            //encodes our json object into bytes and sends our encoded object over the connection to our client
+            var bytes = Encoding.UTF8.GetBytes(establishedJson);
+            await webSocket.SendAsync(bytes, WebSocketMessageType.Text, WebSocketMessageFlags.EndOfMessage, CancellationToken.None);
+
+            Text message = new Text()
+            {
+                Message = "Hello Client!"
+            };
+
+            var json = JsonSerializer.Serialize(message);
+
+            // Receives/Sends data while handshake between client is open
+            while (!webSocket.CloseStatus.HasValue)
+            {
+
+                //Creates and allocates bytes to buffer
+                var buffer = new byte[2048];
+
+                //Receives byte encoded json object from client and fills buffer
+                var content = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+
+                //Grabs encoded json object from buffer and decodes into json string
+                var s = Encoding.UTF8.GetString(buffer, 0, content.Count);
+
+                //Deserializes json string into Text object
+                var receivedJson = JsonSerializer.Deserialize<Text>(s);
+
+                //Writes out our Text objects' content
+                _logger.LogInformation("Message from client: {0} ", receivedJson.Message);
+
+                //Encodes our serialized 'message' Text object we made earlier into bytes
+                var bytes2 = Encoding.UTF8.GetBytes(json);
+
+                //Sends our encoded 'message' over the connection to our client
+                await webSocket.SendAsync(bytes2, WebSocketMessageType.Text, WebSocketMessageFlags.EndOfMessage, CancellationToken.None);
+            }
+
+            //Closes handshake/connection between server/client. Reached if potential client closes connection which causes the whileloop to break
+            await webSocket.CloseAsync(
+                webSocket.CloseStatus.Value,
+                webSocket.CloseStatusDescription,
+                CancellationToken.None);
+        }
+    }
+}
---
+++ WebSocketsUppgift/wwwroot/js/ws.js
@@ -0,0 +1,28 @@
+﻿//Our 'client'
+
+//Establishes connection to our server
+const socket = new WebSocket("ws://localhost:5156/ws");
+
+//Creates object to be serialized
+let messageObject = {
+    Message: "Hello Server!"}
+
+//Serializes object
+let jsonObject = JSON.stringify(messageObject)
+
+//Keeps sending our serialized message object to our server as long as connection is open
+socket.addEventListener('open', (event) => {
+    console.log("Socket open!", event);
+
+    setInterval(() => {
+        socket.send(jsonObject);
+    }, 3000)
+});
+
+//Receives and deserializes message object from server and prints message to console
+socket.addEventListener('message', (event) => {
+
+    const obj = JSON.parse(event.data)
+
+    console.log("Message from server: ", obj.Message);
+});
